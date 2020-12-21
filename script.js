5bdfceccb2c31320e9c583fbe9dd6f3d3cb93aee
john-rodewald <jnrodewald99@gmail.com>
2020-12-21 19:05:19 +0100

    Remove cursor, add close button

--- script.js
+++ script.js
@@ -2,5 +2,4 @@ const onClickJoinGroup = () => {
     const modal = document.querySelector('#gid-iframe');
 
     modal.setAttribute('src', modal.getAttribute('data-src'));
-    console.log('aha!')
 };
