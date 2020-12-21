f930d4c1fb6a292f41ca6da9799658e07c97b695
john-rodewald <jnrodewald99@gmail.com>
2020-12-21 19:28:47 +0100

    Add callback

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ const onClickJoinGroup = () => {
 
     iframe.setAttribute('src', iframe.getAttribute('data-src'));
 };
+
+if (window.addEventListener) {
+  window.addEventListener("message", onMessage, false);
+}
+
+else if (window.attachEvent) {
+  window.attachEvent("onmessage", onMessage, false);
+}
+
+function onMessage(event) {
+  var data = event.data;
+  if (typeof (window[data.func]) == "function") {
+    window[data.func].call(null, data.message);
+  }
+}
+
+// Function to be called from iframe
+function parentFunc(message) {
+  alert(message);
+}
