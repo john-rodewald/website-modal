f7bcdf6c648ad695a70b649eff6fa2c7fe484a4d
john-rodewald <jnrodewald99@gmail.com>
2020-12-23 13:44:29 +0100

    Fix card info spacing, make username customizable

--- script.js
+++ script.js
@@ -52,5 +52,5 @@ function readCookie (name) {
 		while (c.charAt(0)==' ') c = c.substring(1,c.length);
 		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
 	}
-	return '';
+	return null;
 }
